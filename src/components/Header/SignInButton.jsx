ae089d90511d22bad2e490bd8cd5a0671745ea15
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-22 15:29:11 -0500

    edited button color and hover

--- src/components/Header/SignInButton.jsx
+++ src/components/Header/SignInButton.jsx
@@ -5,7 +5,7 @@ const SignInButton = () => {
   return (
     <Link to="/signinpage">
       <button
-        className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-7 rounded text-md focus:outline-none focus:shadow-outline transform transition-colors duration-150 ease-in-out"
+        className="bg-[#1B73E9] hover:bg-blue-600 text-white py-[7px] px-[27px] rounded text-[14px] hover:shadow-xl "
         style={{ fontFamily: "Roboto, sans-serif", fontWeight: 400 }}
       >
         Sign in
