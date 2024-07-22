49fb93fa9e9d3076f84f41f2164052fd35522d57
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-22 14:16:10 -0500

    created signin page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
+import SignInPage from "./SignInPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signinpage" element={<SignInPage />} />
       </Routes>
     </Router>
   );
