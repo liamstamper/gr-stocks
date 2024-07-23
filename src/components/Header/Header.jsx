d0810f144b1c59832947017b6dab31711e22fef1
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-23 10:56:04 -0500

    updateed hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,26 +2,14 @@ import React from "react";
 import Branding from "./Branding";
 import Icon from "./Icon";
 import SignInButton from "./SignInButton";
+import Hamburger from "../../assets/icons/hamburger.png";
 
 const Header = ({ toggleSidebar }) => {
   return (
     <header className="h-[65px] pr-[20px] bg-white border bottom-1 border-gray-50 flex justify-between items-center mx-auto w-full px-4">
       <div className="flex items-center">
         <button className="pr-4" onClick={toggleSidebar}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+          <img src={Hamburger} className="w-6 h-6 "></img>
         </button>
         <Branding />
       </div>
