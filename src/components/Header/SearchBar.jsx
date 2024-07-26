cf3a49b4abfb70851b844b9b6bbea61c55bbd2e3
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-26 13:12:22 -0500

    created searchbar

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import SearchLogo from "../../assets/icons/search.png";
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div className="flex justify-center">
+      <input
+        type="search"
+        className="mt-[31px] border border-gray-200 shadow-md mx-auto rounded-full h-[62px] py-4 px-10 w-[555px] placeholder:text-gray-700 placeholder:pl-2 placeholder:text-[16px] text-black bg-[left_12px_center] bg-no-repeat bg-left-center"
+        style={{
+          fontFamily: "Roboto, sans-serif",
+          fontWeight: 400,
+          backgroundImage: `url(${SearchLogo})`,
+          backgroundSize: "23px 23px",
+        }}
+        placeholder="Search for stocks, ETFs & more"
+      />
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
