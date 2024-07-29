ef232e36aa52aaafc6efcea0f890c9a950d2d3a2
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-29 16:14:31 -0500

    removed hovershadow

--- src/components/Header/SignInButton.jsx
+++ src/components/Header/SignInButton.jsx
@@ -5,7 +5,7 @@ const SignInButton = () => {
   return (
     <Link to="/signinpage">
       <button
-        className="bg-[#1B73E9] hover:bg-blue-600 text-white py-[7px] px-[27px] rounded text-[14px] hover:shadow-xl "
+        className="bg-[#1B73E9] hover:bg-blue-600 text-white py-[7px] px-[27px] rounded text-[14px]"
         style={{ fontFamily: "Roboto, sans-serif", fontWeight: 400 }}
       >
         Sign in
