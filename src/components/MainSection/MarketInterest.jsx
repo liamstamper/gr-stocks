c8d0eea49362a253d4644e6876a2728facb6aeed
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-29 14:12:23 -0500

    styling done

--- src/components/MainSection/MarketInterest.jsx
+++ src/components/MainSection/MarketInterest.jsx
@@ -1,30 +1,113 @@
 import React from "react";
 import InfoIcon from "../../assets/icons/info.png";
+import AddCircle from "../../assets/icons/addcircle.png";
 
-const MarketInterest = () => {
+const MarketInterest = (name, percentage, priceChange, currentPrice, isUp) => {
   return (
-    <div className="flex  pt-[28px]">
+    <div className="flex pt-[28px]">
       <div className="bg-white w-[640px] h-[335px] p-4">
-        <h2 className="text-[18px] pb-2 flex text-gray-800 mb-1 border-b border-spacing-y-3 border-gray-200">
+        <h2 className="text-[18px] pb-2 flex text-gray-800 mb-1 border-b  border-gray-200">
           You may be interested in
           <img class="ml-2 mt-1 w-4 h-4" src={InfoIcon}></img>
         </h2>
         <ul>
-          <li className="flex text-[14px] justify-between items-center py-3 border-b border-gray-200">
-            <span>Amazon.com Inc</span>
-            <span>$182.20</span>
+          <li className="flex py-2 border-b border-gray-200">
+            <div className="text-[14px] py-[7px] pr-[150px] flex">
+              <div className="text-white font-semibold pt-[3px] text-[11px] rounded-md bg-red-600 px-2 mr-2">
+                TLSA
+              </div>
+              Tesla Inc
+            </div>
+            <div className="flex items-right space-x-14">
+              <div className="text-[15px] py-[7px] text-left">$182.20</div>
+              <div className="text-[15px] py-[7px] text-green-700 text-left font-semibold">
+                +$182.20
+              </div>
+              <div className="text-[15px] py-[7px] px-1 text-green-700 font-semibold text-left rounded-lg bg-opacity-30 bg-green-300">
+                18.20%
+              </div>
+            </div>
+            <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
           </li>
-          <li className="flex text-[14px] justify-between items-center py-3 border-b border-gray-200">
-            <span>Amazon.com Inc</span>
-            <span>$182.20</span>
+        </ul>
+        <ul>
+          <li className="flex py-2 border-b border-gray-200">
+            <div className="text-[14px] py-[7px] pr-[150px] flex">
+              <div className="text-white font-semibold pt-[3px] text-[11px] rounded-md bg-blue-800 px-2 mr-2">
+                TLSA
+              </div>
+              Tesla Inc
+            </div>
+            <div className="flex items-right space-x-14">
+              <div className="text-[15px] py-[7px] text-left">$182.20</div>
+              <div className="text-[15px] py-[7px] text-green-700 text-left font-semibold">
+                +$182.20
+              </div>
+              <div className="text-[15px] py-[7px] px-1 text-green-700 font-semibold text-left rounded-lg bg-opacity-30 bg-green-300">
+                18.20%
+              </div>
+            </div>
+            <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
           </li>
-          <li className="flex text-[14px] justify-between items-center py-3 border-b border-gray-200">
-            <span>Amazon.com Inc</span>
-            <span>$182.20</span>
+        </ul>
+        <ul>
+          <li className="flex py-2 border-b border-gray-200">
+            <div className="text-[14px] py-[7px] pr-[150px] flex">
+              <div className="text-white font-semibold pt-[3px] text-[11px] rounded-md bg-black px-2 mr-2">
+                TLSA
+              </div>
+              Tesla Inc
+            </div>
+            <div className="flex items-right space-x-14">
+              <div className="text-[15px] py-[7px] text-left">$182.20</div>
+              <div className="text-[15px] py-[7px] text-red-700 text-left font-semibold">
+                +$182.20
+              </div>
+              <div className="text-[15px] py-[7px] px-1 text-red-700 font-semibold text-left rounded-lg bg-opacity-30 bg-red-300">
+                18.20%
+              </div>
+            </div>
+            <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
           </li>
-          <li className="flex text-[14px] justify-between items-center py-3 border-b border-gray-200">
-            <span>Amazon.com Inc</span>
-            <span>$182.20</span>
+        </ul>
+        <ul>
+          <li className="flex py-2 border-b border-gray-200">
+            <div className="text-[14px] py-[7px] pr-[150px] flex">
+              <div className="text-black border border-black font-semibold pt-[3px] text-[11px] rounded-md bg-white px-2 mr-2">
+                TLSA
+              </div>
+              Tesla Inc
+            </div>
+            <div className="flex items-right space-x-14">
+              <div className="text-[15px] py-[7px] text-left">$182.20</div>
+              <div className="text-[15px] py-[7px] text-red-700 text-left font-semibold">
+                +$182.20
+              </div>
+              <div className="text-[15px] py-[7px] px-1 text-red-700 font-semibold text-left rounded-lg bg-opacity-30 bg-red-300">
+                18.20%
+              </div>
+            </div>
+            <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
+          </li>
+        </ul>
+        <ul>
+          <li className="flex py-2 border-b border-gray-200">
+            <div className="text-[14px] py-[7px] pr-[150px] flex">
+              <div className="text-white font-semibold pt-[3px] text-[11px] rounded-md bg-orange-600 px-2 mr-2">
+                TLSA
+              </div>
+              Tesla Inc
+            </div>
+            <div className="flex items-right space-x-14">
+              <div className="text-[15px] py-[7px] text-left">$182.20</div>
+              <div className="text-[15px] py-[7px] text-green-700 text-left font-semibold">
+                +$182.20
+              </div>
+              <div className="text-[15px] py-[7px] px-1 text-green-700 font-semibold text-left rounded-lg bg-opacity-30 bg-green-300">
+                18.20%
+              </div>
+            </div>
+            <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
           </li>
         </ul>
       </div>
