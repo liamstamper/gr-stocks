ae248fb6b517ffbcbaf13e2a1c912390045aaa3c
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-29 19:25:02 -0500

    quick flix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,11 +2,11 @@ import React, { useRef, useEffect } from "react";
 import Branding from "./Header/Branding";
 
 const SideBar = ({ toggleSidebar }) => {
-  const sidebarRef = useRef();
+  const sidebarRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
-      if (!sidebarRef.current.contains(event.target)) {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
         console.log("Click outside detected");
         toggleSidebar(false);
       }
@@ -18,11 +18,14 @@ const SideBar = ({ toggleSidebar }) => {
     };
   }, [toggleSidebar]);
 
+  const closeBar = () => toggleSidebar(false);
+
   return (
     <aside ref={sidebarRef} className="fixed inset-0 flex">
       <div className="w-64 bg-white p-6 shadow-xl transition ease-in-out">
         <nav className="space-y-8 text-sm">
           <Branding />
+          <button onClick={closeBar}>close</button>
 
           <div className="space-y-10">
             <a className="text-sm font-semibold tracking-wide mt-8">Home</a>
