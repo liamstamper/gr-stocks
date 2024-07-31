cce4f13ed95702e2c07aa45bb27808161da38cac
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-31 16:10:27 -0500

    refactored to add searchbar to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Branding from "./Branding";
 import Icon from "./Icon";
 import SignInButton from "./SignInButton";
 import Hamburger from "../../assets/icons/hamburger.png";
 import AppsImage from "../../assets/icons/apps.png";
+import SearchBarHead from "./SearchBarHead";
 
 const Header = ({ toggleSidebar }) => {
+  const location = useLocation();
+  const isStockPage = location.pathname.startsWith("/stock");
   return (
     <header className="h-[65px] pr-[20px] bg-white border bottom-1 border-gray-50 flex justify-between items-center mx-auto w-full px-2">
       <div className="flex items-center">
@@ -17,6 +21,7 @@ const Header = ({ toggleSidebar }) => {
         </button>
         <Branding />
       </div>
+      {isStockPage ? <SearchBarHead /> : <div></div>}
       <div className="items-center flex-shrink-0 hidden lg:flex">
         <button className="hover:rounded-full hover:bg-gray-100 mr-5 p-2 shadow-gray-400 transition-all duration-150">
           <img src={AppsImage} className="h-6 w-6"></img>
