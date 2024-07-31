d4e1311d728ad2e28cf2881a747f0751d344a89f
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-07-30 19:54:01 -0500

    added martket trends to main section

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import MarketInterest from "./MarketInterest";
 import CreatePorfolio from "./CreatePorfolio";
+import MarketTrends from "./MarketTrends";
 
 const MainSection = () => {
   return (
-    <div className=" flex justify-center">
-      <MarketInterest />
-      <CreatePorfolio />
+    <div className="flex justify-center">
+      <MarketInterest
+        name="Tesla Inc"
+        percentage="21.50"
+        priceChange="10.20"
+        currentPrice="781.00"
+        isUp={true}
+      />
+      <div className="flex flex-col">
+        <CreatePorfolio />
+        <MarketTrends />
+      </div>
     </div>
   );
 };
