e0ccb18694384afb1e83621317087fa0fad86635
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-08-03 16:11:32 -0500

    refactored data

--- src/components/MainSection/MarketInterest.jsx
+++ src/components/MainSection/MarketInterest.jsx
@@ -1,56 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import InfoIcon from "../../assets/icons/info.png";
-import AddCircle from "../../assets/icons/addcircle.png";
-import upArrowImg from "../../assets/icons/uparrow.png";
-import downArrowImg from "../../assets/icons/downarrow.png";
 import MarketInterestBlock from "./MarketInterestBlock";
 
-const MarketInterest = ({}) => {
+const MarketInterest = () => {
+  const stocks = [
+    {
+      name: "Tesla Inc",
+      percentage: "21.50",
+      priceChange: "10.20",
+      currentPrice: "781.00",
+      isUp: true,
+    },
+    {
+      name: "Apple Inc",
+      percentage: "15.30",
+      priceChange: "8.15",
+      currentPrice: "152.10",
+      isUp: false,
+    },
+    {
+      name: "Google LLC",
+      percentage: "10.75",
+      priceChange: "5.00",
+      currentPrice: "2792.35",
+      isUp: true,
+    },
+    // Add more stock data as needed
+  ];
+
   return (
     <div className="flex pt-[28px]">
-      <div className="bg-white w-[640px] h-[335px] p-4">
-        <h2 className="text-[18px] flex text-gray-800 mb-1 border-b pb-2 border-gray-200">
+      <div className="bg-white w-[660px] h-[335px]">
+        <h2 className="text-[18px] flex items-center text-gray-800 mb-1 border-b pb-2 border-gray-200">
           You may be interested in
-          <img class="ml-2 mt-1 w-4 h-4" src={InfoIcon}></img>
+          <img className="ml-2 mt-1 w-4 h-4" src={InfoIcon} alt="Info Icon" />
         </h2>
-        <Link to={`/stockpage/${name}`}>
+        {stocks.map((stock, index) => (
           <MarketInterestBlock
-            name="Tesla Inc"
-            percentage="21.50"
-            priceChange="10.20"
-            currentPrice="781.00"
-            isUp={true}
+            key={index}
+            name={stock.name}
+            percentage={stock.percentage}
+            priceChange={stock.priceChange}
+            currentPrice={stock.currentPrice}
+            isUp={stock.isUp}
           />
-          <MarketInterestBlock
-            name="Tesla Inc"
-            percentage="21.50"
-            priceChange="10.20"
-            currentPrice="781.00"
-            isUp={true}
-          />
-          <MarketInterestBlock
-            name="Tesla Inc"
-            percentage="21.50"
-            priceChange="10.20"
-            currentPrice="781.00"
-            isUp={true}
-          />
-          <MarketInterestBlock
-            name="Tesla Inc"
-            percentage="21.50"
-            priceChange="10.20"
-            currentPrice="781.00"
-            isUp={true}
-          />
-          <MarketInterestBlock
-            name="Tesla Inc"
-            percentage="21.50"
-            priceChange="10.20"
-            currentPrice="781.00"
-            isUp={false}
-          />
-        </Link>
+        ))}
       </div>
     </div>
   );
