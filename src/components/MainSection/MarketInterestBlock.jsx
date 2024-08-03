89b9bfbe507e9ec71041cfdce03978d667ed91e2
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-08-03 16:11:08 -0500

    refactored to make spacing uniform

--- src/components/MainSection/MarketInterestBlock.jsx
+++ src/components/MainSection/MarketInterestBlock.jsx
@@ -13,37 +13,39 @@ const MarketInterestBlock = ({
   isUp,
 }) => {
   return (
-    <li className="flex py-1 border-b border-gray-200">
-      <div className="text-[14px] py-[10px] pr-[150px] flex">
-        <div className="text-white font-semibold pt-[2px] text-[11px] rounded-md bg-yellow-500 px-2 mr-2">
-          TLSA
+    <Link to={`/stockpage/${name}`} className="block">
+      <li className="flex items-center py-3 border-b border-gray-200">
+        <div className="flex-1 flex items-center">
+          <div className="text-white font-semibold text-[11px] rounded-md bg-red-600 px-2 py-1 mr-2">
+            {name.substring(0, 4)}
+          </div>
+          <span className="text-[14px]">{name}</span>
         </div>
-        {name}
-      </div>
-      <div className="flex items-right space-x-16 justify-center">
-        <div className="text-[15px] py-[7px] text-left">${currentPrice}</div>
-        <div
-          className={`text-[15px] py-[7px] px-1 font-semibold text-left rounded-lg bg-opacity-30  ${
-            isUp ? "text-green-700" : "text-red-700"
-          }`}
-        >
-          {isUp ? "+" : "-"}${priceChange}
+        <div className="flex-1 text-[15px] flex items-center justify-between">
+          <div>${currentPrice}</div>
+          <div
+            className={`font-semibold rounded-lg py-1 px-2 ${
+              isUp ? "text-green-700" : " text-red-700"
+            }`}
+          >
+            {isUp ? "+" : "-"}${priceChange}
+          </div>
+          <div
+            className={`flex text-[15px] font-semibold items-center py-1 px-2 bg-opacity-30 rounded-lg ${
+              isUp ? "bg-green-300 text-green-700" : "bg-red-300 text-red-700"
+            }`}
+          >
+            <img
+              src={isUp ? upArrowImg : downArrowImg}
+              alt={isUp ? "Up Arrow" : "Down Arrow"}
+              className="h-3 w-3 opacity-70 mr-1"
+            />
+            {percentage}%
+          </div>
         </div>
-        <div
-          className={`text-[13px] mt-[2px] text-green-700 font-semibold rounded-lg bg-opacity-30 flex flex-row  pt-[7px] ${
-            isUp ? "bg-green-300 text-green-700" : "bg-red-300 text-red-700"
-          }`}
-        >
-          <img
-            src={isUp ? upArrowImg : downArrowImg}
-            alt={isUp ? "Up Arrow" : "Down Arrow"}
-            className="h-3 w-3 opacity-70"
-          />
-          {percentage}%
-        </div>
-      </div>
-      <img className="h-6 w-6 mt-1 ml-3" src={AddCircle}></img>
-    </li>
+        <img className="h-6 w-6 ml-3" src={AddCircle} alt="Add Circle" />
+      </li>
+    </Link>
   );
 };
 
