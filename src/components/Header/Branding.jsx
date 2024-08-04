04c18028098f4f01b195d274eb1fd3d8f5d16026
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-08-04 11:25:27 +0200

    updated hamburger spacing

--- src/components/Header/Branding.jsx
+++ src/components/Header/Branding.jsx
@@ -6,13 +6,11 @@ const Branding = () => {
   return (
     <div className="flex items-center">
       {" "}
-      <Link
-        style={{ fontFamily: "Roboto, sans-serif", fontWeight: 400 }}
-        to="/"
-        className="text-xl font-semibold text-slate-500 tracking-wide flex"
-      >
-        <img alt="Logo" className="w-18 h-6 mr-2 mt-1" src={logo} />
-        <span>Finance</span>
+      <Link to="/" className="flex">
+        <img alt="Logo" className="w-[75px] h-6 mr-[4px] mt-[7px]" src={logo} />
+        <span className="text-[21px] mt-[2px] font-roboto text-slate-500">
+          Finance
+        </span>
       </Link>
     </div>
   );
